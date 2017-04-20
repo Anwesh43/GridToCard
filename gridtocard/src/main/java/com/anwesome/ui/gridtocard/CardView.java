ca20a70e99511e0ae4f6ec31d72ac932e3203053
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 12:11:15 +0530

    Adding CardView

---
+++ gridtocard/src/main/java/com/anwesome/ui/gridtocard/CardView.java
@@ -0,0 +1,78 @@
+package com.anwesome.ui.gridtocard;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by anweshmishra on 20/04/17.
+ */
+public class CardView extends View {
+    private Bitmap bitmap;
+    private float currX,currY,finalX,finalY,scaleX,scaleY,deg=0,xSpeed = 0,ySpeed = 0,dir = 1,scaleYSpeed = 0,scaleXSpeed=0;
+    private float initX,initY,initSx,initSy;
+    private boolean isAnimated = false;
+    public CardView(Context context,Bitmap bitmap) {
+        super(context);
+        this.bitmap = bitmap;
+    }
+    public void onDraw(Canvas canvas) {
+        canvas.save();
+        canvas.translate(currX,currY);
+        canvas.rotate(deg);
+        canvas.scale(scaleX,scaleY);
+        canvas.restore();
+        if(isAnimated) {
+            move();
+            try {
+                Thread.sleep(50);
+                invalidate();
+            }
+            catch (Exception ex) {
+
+            }
+        }
+    }
+    public void move() {
+        deg+=60*dir;
+        currX += xSpeed;
+        currY += ySpeed;
+        scaleX += scaleXSpeed;
+        if((xSpeed>0 && currX>finalX) || (xSpeed<0 && currX<finalX)) {
+            currX = finalX;
+            xSpeed = 0;
+        }
+        if((ySpeed>0 && currY>finalY) || (ySpeed<0 && currY<finalY)) {
+            currY = finalY;
+            ySpeed = 0;
+        }
+        if(xSpeed== 0 && ySpeed == 0) {
+            dir = 0;
+            isAnimated = false;
+        }
+    }
+    public void init(Bitmap bitmap,float cx,float cy,int w,int h) {
+        this.bitmap = Bitmap.createScaledBitmap(bitmap,w,h,true);
+        finalX = w;
+        finalY = h;
+        initX = cx;
+        initY = cy;
+        currX = cx;
+        currY = cy;
+        xSpeed = (finalX-currX)/6;
+        ySpeed = (finalY-currY)/6;
+        initSx = bitmap.getWidth()/(w*1.0f);
+        initSy = bitmap.getHeight()/(h*1.0f);
+        scaleX = initSx;
+        scaleY = initSy;
+        scaleXSpeed = (1-scaleX)/6;
+        scaleYSpeed = (1-scaleY)/6;
+        dir = 1;
+        isAnimated = true;
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+}
