6ead5d06f2ae08adcb293ce1bc0b672d75f70baa
Anwesh43 <anweshthecool0@gmail.com>
2017-04-20 17:39:24 +0530

    Using GridToCard in app

--- app/src/main/java/com/anwesome/ui/gridtocarddemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/gridtocarddemo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.anwesome.ui.gridtocarddemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.gridtocard.GridToCard;
 
+public class MainActivity extends AppCompatActivity {
+    int resources[] = {R.drawable.back1,R.drawable.back2,R.drawable.back3,R.drawable.back4};
+    Bitmap bitmaps[] = new Bitmap[4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0;i<resources.length;i++) {
+            bitmaps[i] = BitmapFactory.decodeResource(getResources(),resources[i]);
+        }
+
+        GridToCard gridToCard = new GridToCard(this);
+        for(int i=0;i<24;i++) {
+            gridToCard.addImage(bitmaps[i%4]);
+        }
+        gridToCard.show();
     }
+
 }
